65f55508596e4d46c6ffd738ffe1d4d95a2217d2
mujeeb909 <65164185+mujeeb909@users.noreply.github.com>
2024-01-07 22:30:13 +0500

    Pages and router added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Error from "./pages/Error";
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold py-2">Hello world!</h1>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold py-2 underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ Client/src/pages/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <div>No Page found!!</div>;
+};
+
+export default Error;
